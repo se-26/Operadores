99227da3f596909cc24714b1bf6e014fcc7a5ff6
selena <selenamolinafigueroa26@gmail.com>
2026-03-08 10:50:33 -0500

    enlace los ejercicios del punto D al index

---
+++ 4.js
@@ -0,0 +1,16 @@
+//Ejercicio 4: Estadísticas de calificaciones
+// Constantes
+const nota1 = 4.5;
+const nota2 = 3.8;
+const nota3 = 2.9;
+const nota4 = 4.0;
+
+// Variables
+let suma = nota1 + nota2 + nota3 + nota4;
+let promedio = suma / 4;
+let resultado = (nota1 + nota2) * (nota3 + nota4) / 2;
+
+// Resultados
+console.log("Suma de notas:", suma);
+console.log("Promedio:", promedio);
+console.log("Resultado de la operación:", resultado);
